2f2ff9b9149bd1cb5941d71933e9012733abb7af
Georg Niggli <jornig@outlook.com>
2023-10-15 17:46:16 +0200

    update

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ from heading import calculate_new_heading
 from constants import constants as c
 from map_geojson import lake
 from animate import generate_path
+import time
 
 
 
@@ -20,23 +21,44 @@ def simulate():
     boat_vector = normalize(c.GOAL - c.START)
     wind_direction_vector = normalize(vectorize_angle(c.WIND_DIRECTION))
     goal_vector = normalize(c.GOAL - c.BOAT)
-    goal_heading = vectorize_angle(goal_vector)
     (x, y) = calculate_closest_point_on_line(c.BOAT, c.START, c.GOAL)[2:]
     min_index = calculate_closest_point_on_line(c.BOAT, c.START, c.GOAL)[1]
-    generate_path(boat_vector, wind_direction_vector, goal_vector, 0.001)
-   # new_heading = calculate_new_heading(
- #       boat_vector, wind_direction_vector, goal_vector)
-   # visualize(c.START, c.GOAL, c.BOAT, x, y, min_index,
-   #           wind_direction_vector, new_heading, goal_vector, lake.LAKE)
+   # generate_path(boat_vector, wind_direction_vector, goal_vector, 0.001)
+    new_heading = calculate_new_heading(
+        boat_vector, wind_direction_vector, goal_vector)
+    visualize(c.START, c.GOAL, c.BOAT, x, y, min_index,
+              wind_direction_vector, new_heading, goal_vector, lake.LAKE)
   
+
+def run(boat_vector, boat_location, wind_direction_vector, goal_vector, interval, simulate=True):
+   
+    new_heading = calculate_new_heading(boat_vector, wind_direction_vector, goal_vector)
+    if simulate:
+        (x, y) = calculate_closest_point_on_line(boat_location, c.START, c.GOAL)[2:]
+        min_index = calculate_closest_point_on_line(boat_location, c.START, c.GOAL)[1]
+        print(boat_location)
+        print(boat_vector)
+        # add boat vector to boat location to get new boat location using nupy vector addition
+        boat_location = boat_location + new_heading * 0.001
+        goal_vector = normalize(c.GOAL - boat_location)
+        visualize(c.START, c.GOAL, boat_location, x, y, min_index,
+                  wind_direction_vector, new_heading, goal_vector, lake.LAKE)
+        
+    
+    
+   
+    time.sleep(interval)
+    run(boat_vector, c.BOAT, wind_direction_vector, goal_vector, 1, simulate)
+      
 
 
 def main():
+    boat_vector = normalize(c.GOAL - c.START)
+    wind_direction_vector = normalize(vectorize_angle(c.WIND_DIRECTION))
+    goal_vector = normalize(c.GOAL - c.BOAT)
+   # run(boat_vector, c.BOAT, wind_direction_vector, goal_vector, 1, True)
     simulate()
-    
 
 
 
-
-if __name__ == "__main__":
-    main()
+main()
