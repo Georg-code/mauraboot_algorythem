d665b3dde2d234f407b15651c2290c681c6dee8e
Georg Niggli <54809379+Georg-code@users.noreply.github.com>
2024-01-09 15:50:59 +0000

    commit

--- main.py
+++ main.py
@@ -41,6 +41,16 @@ def main():
 
                 gps_lat = data['gps_lat']
                 gps_lng = data['gps_lng']
+                compass_azimuth = data['compass_azimuth']
+                wind_raw = data['wind_rotation_raw']
+                
+                # wind_raw has a range of 0-1023
+                # we want to convert this to an angle in radias
+                # we will use the formula: angle = (wind_raw / 1023) * 2 * pi
+                
+                wind_vector = vectorize_angle((wind_raw / 1023) * 2 * np.pi)
+
+                compass_azimuth = np.deg2rad(compass_azimuth)
 
                 # make an np array of the gps data
                 gps_data = np.array([gps_lat, gps_lng])
@@ -50,14 +60,33 @@ def main():
 
 
 
-
                 boat_vector = normalize(c.GOAL - c.START)
-                wind_direction_vector = normalize(vectorize_angle(c.WIND_DIRECTION))
+                wind_direction_vector = normalize(wind_vector)
                 goal_vector = normalize(c.GOAL - gps_data)
                 optimal_heading = run(boat_vector, wind_direction_vector, goal_vector)
-                actual_heading = vectorize_angle(data['compass_azimuth'])
+                actual_heading = vectorize_angle(compass_azimuth)
                 print(f'Optimal Heading: {optimal_heading}')
                 print(f'Actual Heading: {actual_heading}')
+
+                # write a pd controller for a servo. The servo is a linear servo connected to the rudder of a boat. Anything over the middle is turning right and anything under the middle is turning left. Controll the servo with gpiozero
+                p = 0.5
+                d = 0.5
+                error = actual_heading - optimal_heading
+                derivative = error - d
+                correction = p * error + d * derivative
+                print(f'Error: {error}')
+                print(f'Correction: {correction}')
+
+                # servo value is between 0 and 1
+                servo_value = 0.5 + correction
+                print(f'Servo Value: {servo_value}')
+
+                
+                    
+      
+
+
+
             
 
                 
